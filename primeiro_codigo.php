1518ba9bc306acbbb5994b4bf2ec53ae6a434572
antoniocvjr <antoniovieira783@gmail.com>
2023-07-29 23:46:15 -0300

    First Commit

---
+++ primeiro_codigo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Primeiro PHP</title>
+</head>
+<body>
+
+<h1>
+<?php
+
+    echo "Olá, Mundo!!";
+    print "Olá Mundo \u{1F5A5}";
+
+
+
+
+
+?>
+<h1>
+
+<p> Vamos nos livrar disso?</p>
+    
+</body>
+</html>
